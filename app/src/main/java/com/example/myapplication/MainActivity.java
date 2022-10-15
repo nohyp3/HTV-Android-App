442c6a182e147103368c4018463f2bbe1b7f315d
Andre <ake05814@gmail.com>
2022-10-14 23:55:21 -0400

    comment

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.EditText;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //View firstNumber = findViewById(R.id.number1);
+        //View secondNumber = findViewById(R.id.number2);
+
     }
 }
