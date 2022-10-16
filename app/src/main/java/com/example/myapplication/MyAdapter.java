723ac120e06a2e98eacbd7cd315aef11acc1fb90
treshhh23 <cameronlin77@gmail.com>
2022-10-15 20:11:53 -0400

    classes

---
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Workouts> workoutsArrayList;
+
+
+    public MyAdapter() {
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
