b54d1fb7e68b5957da2e037452994d9dda2befc9
Andre <ake05814@gmail.com>
2022-10-15 22:51:15 -0400

    added more basic functionality like a running page, and connected the activities through buttons

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -25,10 +25,79 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView click_image_id;
     Button camera;
+    TextView quote;
     private Button cameraPerm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String[] quotes = {"Every strike brings me closer to the next home run. – Babe Ruth"
+                ,"I don’t walk away from things I think are unfinished. – Arnold Schwarzenegger"
+                ,"Whether you think you can or you think you can’t, you’re right. –Henry Ford"
+                ,"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed. –Michael Jordan"
+                ,"I didn’t fail the test. I just found 100 ways to do it wrong. –Benjamin Franklin"
+                ,"A person who never made a mistake never tried anything new. – Albert Einstein"
+                ,"It is never too late to be what you might have been. –George Eliot"
+                ,"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover. –Mark Twain"
+                ,"The most common way people give up their power is by thinking they don’t have any. –Alice Walker"
+                ,"The mind is everything. What you think you become.  –Buddha"
+                ,"Either you run the day, or the day runs you. –Jim Rohn"
+                ,"Life shrinks or expands in proportion to one’s courage. –Anais Nin"
+                ,"Every strike brings me closer to the next home run. – Babe Ruth"
+                ,"I don’t walk away from things I think are unfinished. – Arnold Schwarzenegger"
+                ,"Whether you think you can or you think you can’t, you’re right. –Henry Ford"
+                ,"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed. –Michael Jordan"
+                ,"I didn’t fail the test. I just found 100 ways to do it wrong. –Benjamin Franklin"
+                ,"A person who never made a mistake never tried anything new. – Albert Einstein"
+                ,"It is never too late to be what you might have been. –George Eliot"
+                ,"Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover. –Mark Twain"
+                ,"The most common way people give up their power is by thinking they don’t have any. –Alice Walker"
+                ,"The mind is everything. What you think you become.  –Buddha"
+                ,"Either you run the day, or the day runs you. –Jim Rohn"
+                ,"When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us. –Helen Keller"
+                ,"Everything has beauty, but not everyone can see. –Confucius"
+                ,"You only live once, but if you do it right, once is enough. — Mae West"
+                ,"Many of life’s failures are people who did not realize how close they were to success when they gave up.– Thomas A. Edison"
+                ,"If you want to live a happy life, tie it to a goal, not to people or things.– Albert Einstein"
+                ,"Money and success don’t change people; they merely amplify what is already there. — Will Smith"
+                ,"Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking. – Steve Jobs"
+                ,"Not how long, but how well you have lived is the main thing. — Seneca"
+                ,"If life were predictable it would cease to be life, and be without flavour. – Eleanor Roosevelt"
+                ,"The best revenge is massive success. –Frank Sinatra"
+                ,"The person who says it cannot be done should not interrupt the person who is doing it. –Chinese Proverb"
+                ,"Fall seven times and stand up eight. –Japanese Proverb"
+                ,"Never let the fear of striking out keep you from playing the game.– Babe Ruth"
+                ,"You can’t use up creativity.  The more you use, the more you have. –Maya Angelou"
+                ,"I would rather die of passion than of boredom. –Vincent van Gogh"
+                ,"Whatever the mind of man can conceive and believe, it can achieve. –Napoleon Hill"
+                ,"Strive not to be a success, but rather to be of value. –Albert Einstein"
+                ,"What’s money? A man is a success if he gets up in the morning and goes to bed at night and in between does what he wants to do. –Bob Dylan"
+                ,"There are no traffic jams along the extra mile. –Roger Staubach"
+                ,"Teach thy tongue to say, “I do not know,” and thous shalt progress. –Maimonides"
+                ,"We become what we think about. –Earl Nightingale"
+                ,"Winning isn’t everything, but wanting to win is. –Vince Lombardi"
+                ,"I am not a product of my circumstances. I am a product of my decisions. –Stephen Covey"
+                ,"If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on. –Sheryl Sandberg"
+                ,"The whole secret of a successful life is to find out what is one’s destiny to do, and then do it.– Henry Ford"
+                ,"Not how long, but how well you have lived is the main thing. — Seneca"
+                ,"If life were predictable it would cease to be life, and be without flavour. – Eleanor Roosevelt"
+                ,"The best revenge is massive success. –Frank Sinatra"
+                ,"The person who says it cannot be done should not interrupt the person who is doing it. –Chinese Proverb"
+                ,"Fall seven times and stand up eight. –Japanese Proverb"
+                ,"Never let the fear of striking out keep you from playing the game.– Babe Ruth"
+                ,"You can’t use up creativity.  The more you use, the more you have. –Maya Angelou"
+                ,"I would rather die of passion than of boredom. –Vincent van Gogh"
+                ,"Whatever the mind of man can conceive and believe, it can achieve. –Napoleon Hill"
+                ,"Strive not to be a success, but rather to be of value. –Albert Einstein"
+                ,"What’s money? A man is a success if he gets up in the morning and goes to bed at night and in between does what he wants to do. –Bob Dylan"
+                ,"I attribute my success to this: I never gave or took any excuse. –Florence Nightingale"
+                ,"There are no traffic jams along the extra mile. –Roger Staubach"
+                ,"Teach thy tongue to say, “I do not know,” and thous shalt progress. –Maimonides"
+                ,"We become what we think about. –Earl Nightingale"
+                ,"Winning isn’t everything, but wanting to win is. –Vince Lombardi"
+                ,"I am not a product of my circumstances. I am a product of my decisions. –Stephen Covey"
+                ,"If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on. –Sheryl Sandberg"
+                ,"The whole secret of a successful life is to find out what is one’s destiny to do, and then do it.– Henry Ford"};
+        int randomNum = (int)(Math.random()*quotes.length);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -49,6 +118,9 @@ public class MainActivity extends AppCompatActivity {
             // Start the activity with camera_intent, and request pic id
             startActivityForResult(camera_intent, picId);
         });
+        quote = findViewById(R.id.quote);
+        quote.setText(quotes[randomNum]);
+
     }
     // Function to check and request permission.
     public void checkPermission(String permission, int requestCode)
@@ -91,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void changeView(View view){
-        Intent intent = new Intent(this,MainActivity3.class);
+        Intent intent = new Intent(this,MainActivity2.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,13 +2,19 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    public void camera(View view){
+        Intent intent = new Intent(this,MainActivity3.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
     }
+
 }
